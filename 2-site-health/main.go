fec54aae7ab4831d6071946683c0d6d0a7000c3c
Serhii Bohdanov <serhii.bohdanov@student.tuke.sk>
2026-04-25 21:51:10 +0200

    feat: come concurrency

--- 2-site-health/main.go
+++ 2-site-health/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 )
 
 var URLS_FILE_PATH = "websites.txt"
@@ -18,15 +19,36 @@ func readUrls() ([]string, error) {
 	return strings.Fields(string(data)), nil
 }
 
-func chechWebSite(url string) bool{
+func chechWebSite(url string, status_map *Statuses, wg *sync.WaitGroup) {
 	
+	defer wg.Done()
+
 	response, err:=http.Get(url)
 
-	if err != nil || response.StatusCode != 200{
-		return false
-	}
-	return true
+	status_map.mu.Lock()
+	defer status_map.mu.Unlock()
 
+	if err != nil || response.StatusCode != 200{
+		status_map.statuses[url] = false
+		return
+	}
+	status_map.statuses[url] = true
+
+}
+
+type Statuses struct {
+	mu sync.Mutex
+	statuses map[string]bool
+}
+
+func (statuses *Statuses) Print(){
+	for url,status := range statuses.statuses {
+		if status{
+			fmt.Printf("%s - ok\n", url)
+		}else{
+			fmt.Printf("%s - down\n", url)
+		}
+	}
 }
 
 
@@ -37,13 +59,18 @@ func main(){
 		log.Fatal(err)
 	}
 
+	wsStatus := Statuses{statuses:make(map[string]bool)}
+
+	var wg sync.WaitGroup
+
 
 	for _,url:=range urls{
-		if chechWebSite(url){
-			fmt.Printf("%s - ok\n", url)
-		}else{
-			fmt.Printf("%s - down\n", url)
-		}
+		wg.Add(1)
+		go chechWebSite(url, &wsStatus, &wg)
 	}
+	wg.Wait()
+
+	wsStatus.Print()
+	
 
 }
